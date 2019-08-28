c08a62db3cecf99c72802f6eaa33de745279223f
Lorenzo Addazi <lorenzo.addazi@mdh.se>
2019-08-28 16:38:15 +0200

    mpi - lab3 - beskow
    Signed-off-by: Lorenzo Addazi <lorenzo.addazi@mdh.se>

--- lab3/hello_mpi.c
+++ lab3/hello_mpi.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <mpi.h>
 
+#define BUFFER_SIZE 50
+
 int main (int argc, char *argv[])
 {
   int rank, 
       size,
       int_size;
   
-  char buffer[30];
+  char buffer[BUFFER_SIZE];
 
   MPI_File file;
 
@@ -19,7 +21,8 @@ int main (int argc, char *argv[])
   MPI_File_open(MPI_COMM_WORLD, "hello_mpi.bin", MPI_MODE_WRONLY + MPI_MODE_CREATE, MPI_INFO_NULL, &file);
 
   sprintf(buffer, "(%d/%d) : Hello, World!\n", rank + 1, size);
-  MPI_File_write_at(file, rank * 20, &buffer, 20, MPI_CHAR, MPI_STATUS_IGNORE);
+  MPI_File_write_at(file, rank * BUFFER_SIZE, &buffer, BUFFER_SIZE, MPI_CHAR, 
+MPI_STATUS_IGNORE);
 
   MPI_File_close(&file);
 
