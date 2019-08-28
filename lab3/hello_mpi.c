b89c0b2d81162e4d9022520aaa4c30e8503ef443
Lorenzo Addazi <lorenzo.addazi@mdh.se>
2019-08-28 16:08:16 +0200

    mpi - lab3 - local
    Signed-off-by: Lorenzo Addazi <lorenzo.addazi@mdh.se>

--- lab3/hello_mpi.c
+++ lab3/hello_mpi.c
@@ -3,13 +3,24 @@
 
 int main (int argc, char *argv[])
 {
-  int myrank, size;
+  int rank, 
+      size,
+      int_size;
+  
+  char buffer[] = "Hello, World!\n";
+
+  MPI_File file;
 
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  printf("Processor %d of %d: Hello World!\n", myrank, size);
-
+  MPI_Type_size(MPI_INT, &int_size);  
+  MPI_File_open(MPI_COMM_WORLD, "/tmp/out.bin", MPI_MODE_WRONLY + MPI_MODE_CREATE, MPI_INFO_NULL, &file);
+  MPI_File_write_at(file, 
+    rank * (sizeof(buffer)/sizeof(buffer[0])), 
+    &buffer, (sizeof(buffer)/sizeof(buffer[0])),
+     MPI_CHAR, MPI_STATUS_IGNORE);
+  MPI_File_close(&file);
   MPI_Finalize();
 
   return 0;
