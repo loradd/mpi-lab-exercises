d21971fb95567fee603ed92dc8a63afd82b18403
Lorenzo Addazi <lorenzo.addazi@mdh.se>
2019-08-28 16:30:37 +0200

    mpi - lab3 - local
    Signed-off-by: Lorenzo Addazi <lorenzo.addazi@mdh.se>

--- lab3/hello_mpi.c
+++ lab3/hello_mpi.c
@@ -7,7 +7,7 @@ int main (int argc, char *argv[])
       size,
       int_size;
   
-  char buffer[] = "Hello, World!\n";
+  char buffer[30];
 
   MPI_File file;
 
@@ -15,13 +15,14 @@ int main (int argc, char *argv[])
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Type_size(MPI_INT, &int_size);  
-  MPI_File_open(MPI_COMM_WORLD, "/tmp/out.bin", MPI_MODE_WRONLY + MPI_MODE_CREATE, MPI_INFO_NULL, &file);
-  MPI_File_write_at(file, 
-    rank * (sizeof(buffer)/sizeof(buffer[0])), 
-    &buffer, (sizeof(buffer)/sizeof(buffer[0])),
-     MPI_CHAR, MPI_STATUS_IGNORE);
-  MPI_File_close(&file);
-  MPI_Finalize();
 
+  MPI_File_open(MPI_COMM_WORLD, "hello_mpi.bin", MPI_MODE_WRONLY + MPI_MODE_CREATE, MPI_INFO_NULL, &file);
+
+  sprintf(buffer, "(%d/%d) : Hello, World!\n", rank + 1, size);
+  MPI_File_write_at(file, rank * 20, &buffer, 20, MPI_CHAR, MPI_STATUS_IGNORE);
+
+  MPI_File_close(&file);
+
+  MPI_Finalize();
   return 0;
 }
